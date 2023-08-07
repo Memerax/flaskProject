17a6af7e9f982719692e286275db0920b11a3cc2
memerax <josephwhite@gmx.com>
2023-08-07 18:47:15 -0230

    Changed Layout to bootstrap template

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
-from flask import Flask
+from flask import Flask, redirect, url_for, request, render_template
+# from forms import BeneficiaryForm
+import pandas as pd
+
 app = Flask(__name__)
+
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Hello World!'
+    return render_template('index.html')
+
 
 if __name__ == '__main__':
     app.run()
