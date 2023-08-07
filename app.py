d13b71402f021c24215f1e2cfb4db5c2a9bf5532
memerax <josephwhite@gmx.com>
2023-08-07 19:58:05 -0230

    Added titles and linkage for nav bar

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, redirect, url_for, request, render_template
-# from forms import BeneficiaryForm
+from forms import BeneficiaryForm
 import pandas as pd
 
 app = Flask(__name__)
@@ -12,23 +12,23 @@ def home():
 
 @app.route('/about')
 def about():
-    return render_template('about.html')
+    return render_template('about.html', title="About")
 
 @app.route('/add_recipe')
 def add_recipe():
-    return render_template('add_recipe.html')
+    return render_template('add_recipe.html', title="Add a recipe")
 
 @app.route('/remove_recipe')
 def remove_recipe():
-    return render_template('remove_recipe.html')
+    return render_template('remove_recipe.html', title="Remove a recipe")
 
 @app.route('/contact')
 def contact():
-    return render_template('contact.html')
+    return render_template('contact.html',title="Contact page")
 
 @app.route('/search')
 def search():
-    return render_template('search.html')
+    return render_template('search.html',title="Search page")
 
 if __name__ == '__main__':
     app.run()
